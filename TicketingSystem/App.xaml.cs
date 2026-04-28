3af7b7c389c31c736a88fe59b9e849325c5b61a8
marcos jimenez <marcosjv1644@gmail.com>
2026-04-28 18:44:13 +0200

    first commit

---
+++ TicketingSystem/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TicketingSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
